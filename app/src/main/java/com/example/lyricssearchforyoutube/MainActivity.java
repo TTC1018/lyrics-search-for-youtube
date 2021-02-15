3d39ba7112c7a91ac42223dbf22b1a4638b1ca2d
SeoHee3478 <seohee3478@gmail.com>
2021-02-15 21:36:15 +0900

    button location & launch YouTube

--- app/src/main/java/com/example/lyricssearchforyoutube/MainActivity.java
+++ app/src/main/java/com/example/lyricssearchforyoutube/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.lyricssearchforyoutube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Intent intent;
+    private final String packageName = "com.google.android.youtube";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = this.getPackageManager().getLaunchIntentForPackage(packageName);
+
+        ImageButton imagebtn = (ImageButton) findViewById(R.id.imagebtn1);
+        imagebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(intent);
+
+            }
+        });
     }
 }
