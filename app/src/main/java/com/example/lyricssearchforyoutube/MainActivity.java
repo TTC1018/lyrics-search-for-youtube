6a1f1e183a6cd084029635b3355b48f7532ec6e4
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-02-19 15:32:52 +0900

    다른 앱 위에 표시 Dialog 추가

--- app/src/main/java/com/example/lyricssearchforyoutube/MainActivity.java
+++ app/src/main/java/com/example/lyricssearchforyoutube/MainActivity.java
@@ -1,30 +1,104 @@
 package com.example.lyricssearchforyoutube;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private Intent intent;
-    private final String packageName = "com.google.android.youtube";
+
+    private ImageButton startBtn;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intent = this.getPackageManager().getLaunchIntentForPackage(packageName);
+        startBtn = findViewById(R.id.startBtn);
 
-        ImageButton imagebtn = (ImageButton) findViewById(R.id.imagebtn1);
-        imagebtn.setOnClickListener(new View.OnClickListener(){
+        if(isMyServiceRunning()){
+            //이미 플로팅 위젯 실행중일 때 종료
+        }
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity.this.startActivity(intent);
+                // 다른 앱 위에 그리기 권한 체크
+                if(checkOverlayDisplayPermission()){
+                    //플로팅 위젯 시작
 
+                    //본 앱은 종료
+                    finish();
+                }
+                else{
+                    //권한이 없으므로 설정으로 전송
+                    requestOverlayDisplayPermission();
+                }
             }
         });
+
+    }
+
+    private boolean isMyServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        // 현재 실행중인 어플 중 플로팅 위젯이 실행중인지를 판단
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+//            if (FloatingWindowGFG.class.getName().equals(service.service.getClassName())) {
+//                return true;
+//            }
+        }
+        return false;
+    }
+
+    private void requestOverlayDisplayPermission() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setCancelable(true);
+        builder.setTitle("다른 앱 위에 표시");
+        builder.setMessage("'다른 앱 위에 표시' 권한이 필요합니다.");
+        // The event of the Positive-Button is set
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 설정으로 이동
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, RESULT_OK);
+            }
+        });
+        dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.BLACK);
+            }
+        });
+        dialog.show();
+    }
+
+    private boolean checkOverlayDisplayPermission() {
+        // 안드로이드 마쉬멜로우 or API 23 이하 버전은
+        // 다른 앱 위에 그리기 권한 필요없음
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            // '다른 앱 위에 그리기 권한' 유효성 판단
+            if (!Settings.canDrawOverlays(this)) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
     }
 }
