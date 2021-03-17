cfb4d955aa808da86788d9c3c867706631d5a22d
Leewongi <wglee0731@naver.com>
2021-03-17 18:46:45 +0900

    Model 분리

---
+++ app/src/main/java/com/example/lyricssearchforyoutube/data/Music.java
@@ -0,0 +1,32 @@
+package com.example.lyricssearchforyoutube.data;
+
+public class Music {
+    private String title;
+    private String artist;
+    private String lyricsLink;
+
+    public Music(){
+        this.title = "";
+        this.artist = "";
+        this.lyricsLink = "";
+    }
+
+    public Music(String title, String artist, String lyricsLink) {
+        this.title = title;
+        this.artist = artist;
+        this.lyricsLink = lyricsLink;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getLyricsLink() {
+        return lyricsLink;
+    }
+}
