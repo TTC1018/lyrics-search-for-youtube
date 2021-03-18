ed61242a86f43c980cdd2141385ef3265057be25
Leewongi <wglee0731@naver.com>
2021-03-19 03:35:02 +0900

    Splash 화면 추가

---
+++ app/src/main/java/com/example/lyricssearchforyoutube/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.lyricssearchforyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startLoading();
+    }
+
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
